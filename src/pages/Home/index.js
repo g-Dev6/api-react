8657b72f8017955acfeccab75bdf61d62a31287e
Gabriel Dev <bielcubevis@outlook.com>
2025-11-06 15:21:10 -0300

    feat: styling with static data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,56 @@
-import { Container } from "./styles";
+import { Container, MovieList, Movie } from "./styles";
 
 function home() {
+
+    const movies = [
+        {
+            id: 1,
+            title: 'Spider Man',
+            image_url: 'https://th.bing.com/th/id/OIP.twsAmsYpRhmk-4Cbf0QiswHaK_?w=205&h=304&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
+            // link: 'https://google.com.br'
+             
+        },
+        {
+            id: 2,
+            title: 'Batman',
+            image_url: 'https://th.bing.com/th/id/OIP.twsAmsYpRhmk-4Cbf0QiswHaK_?w=205&h=304&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
+            // link: 'https://google.com.br'
+        },
+        {
+            id: 3,
+            title: 'Transformns',
+            image_url: 'https://th.bing.com/th/id/OIP.twsAmsYpRhmk-4Cbf0QiswHaK_?w=205&h=304&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
+            // link: 'https://google.com.br'
+        },
+     ];
+
     return (
         <Container>
             <div>
                 <h1>Movies</h1>
-                <ul>
-                    <li>
-                        <img src="https://th.bing.com/th/id/OIP.twsAmsYpRhmk-4Cbf0QiswHaK_?w=205&h=304&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="spider" />
+                <MovieList>
+
+                    {movies.map((movie) => (
+                        <Movie key={movie.id}>
+                            <a href="https://google.com.br"><img src={movie.image_url} alt={movie.title} /></a>
+                            <span>{movie.title}</span>
+                        </Movie>
+                    ))}
+                    {/* <Movie>
+                       <a href="https://google.com.br"><img src="https://th.bing.com/th/id/OIP.twsAmsYpRhmk-4Cbf0QiswHaK_?w=205&h=304&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="spider" /></a>
                         <span>Spider Man</span>
-                    </li>
-                    <li>
-                        <img src="https://th.bing.com/th/id/OIP.twsAmsYpRhmk-4Cbf0QiswHaK_?w=205&h=304&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="spider" />
+                    </Movie>
+
+                    <Movie>
+                        <a href="https://google.com.br"><img src="https://th.bing.com/th/id/OIP.twsAmsYpRhmk-4Cbf0QiswHaK_?w=205&h=304&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="spider" /></a>
                         <span>Spider Man</span>
-                    </li>
-                    <li>
-                        <img src="https://th.bing.com/th/id/OIP.twsAmsYpRhmk-4Cbf0QiswHaK_?w=205&h=304&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="spider" />
+                    </Movie>
+
+                    <Movie>
+                        <a href="https://google.com.br"><img src="https://th.bing.com/th/id/OIP.twsAmsYpRhmk-4Cbf0QiswHaK_?w=205&h=304&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="spider" /></a>
                         <span>Spider Man</span>
-                    </li>
-                </ul>
+                    </Movie> */}
+                </MovieList>
             </div>
         </Container>
     );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,30 +5,39 @@ export const Container = styled.div`
     text-align: center;
     margin: 4rem 0;
   }
+`;
 
-  ul {
-    list-style: none;
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-    column-gap: 3rem;
-    row-gap: 4rem;
- }
+export const MovieList = styled.ul`
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  column-gap: 3rem;
+  row-gap: 4rem;
+`;
 
- li {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    }
+export const Movie = styled.li`
 
- img {
+display: flex;
+flex-direction: column;
+align-items: center;
+
+img {
     width: 180px;
     border-radius: 1rem;
     margin-bottom: 2rem;
- }
+  }
 
- span {
+  span {
     font-weight: bold;
- }
+    font-size: 120%;
+  }
 
-
+  a {
+    transition: all 0.3s ease-in-out;
+  }
+  
+  a:hover {
+    transform: scale(1.1);
+  }
+  
 `;
