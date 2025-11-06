e58820c49fb8684fb46d6a4956bca40d14623826
Gabriel Dev <bielcubevis@outlook.com>
2025-11-06 14:59:10 -0300

    feat: install styled-components and home css started

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,26 @@
+import { Container } from "./styles";
+
 function home() {
     return (
-        <div>
-            <h1>Movies</h1>
-        </div>
+        <Container>
+            <div>
+                <h1>Movies</h1>
+                <ul>
+                    <li>
+                        <img src="https://th.bing.com/th/id/OIP.twsAmsYpRhmk-4Cbf0QiswHaK_?w=205&h=304&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="spider" />
+                        <span>Spider Man</span>
+                    </li>
+                    <li>
+                        <img src="https://th.bing.com/th/id/OIP.twsAmsYpRhmk-4Cbf0QiswHaK_?w=205&h=304&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="spider" />
+                        <span>Spider Man</span>
+                    </li>
+                    <li>
+                        <img src="https://th.bing.com/th/id/OIP.twsAmsYpRhmk-4Cbf0QiswHaK_?w=205&h=304&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="spider" />
+                        <span>Spider Man</span>
+                    </li>
+                </ul>
+            </div>
+        </Container>
     );
 }
 
